d782f568d44100d0a275e64e2be27f91328fa8d4
Mathilde Arconte <signup@marconte.fr>
2025-02-24 14:53:05 +0100

    google tag manager

--- gatsby-config.js
+++ gatsby-config.js
@@ -72,5 +72,22 @@ module.exports = {
         icon: `src/images/THATMUCH_Logo_Black.png`, // Path to your favicon
       },
     },
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "G-EGCDBV43KT",
+
+        // Include GTM in development.
+        //
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        //
+        // Defaults to null
+        defaultDataLayer: { platform: "gatsby" },
+      },
+    },
   ],
 };
