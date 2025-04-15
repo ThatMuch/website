f7ee7b8c93da1d61abd84dff1b94c20a66d13c42
Mathilde Arconte <signup@marconte.fr>
2025-04-15 15:58:20 +0200

    refactor(related-posts): simplifier la logique de récupération des articles similaires

--- src/components/RelatedPosts/RelatedPosts.tsx
+++ src/components/RelatedPosts/RelatedPosts.tsx
@@ -1,10 +1,9 @@
 import "./RelatedPosts.scss";
 
-import React, { useEffect, useState } from "react";
-
 import PostCard from "../PostCard/PostCard";
 import { PostType } from "../../utils/types";
-import { useBlogCategories } from "../../hooks/use-blog-categories";
+import React from "react";
+import { useSitePosts } from "../../hooks/use-site-posts";
 
 type Props = {
   category: string;
@@ -12,24 +11,18 @@ type Props = {
 };
 
 export default function RelatedPosts({ category, currentPostId }: Props) {
-  const [posts, setPosts] = useState([]);
-  const cats = useBlogCategories();
-  useEffect(() => {
-    const postsFiltered = cats
-      .filter((cat) => cat.slug === category)[0]
-      ?.posts?.nodes.slice(0, 3)
-      .filter((post) => post.id !== currentPostId);
-
-    setPosts(postsFiltered);
-  }, [category]);
-
+  const posts = useSitePosts(category);
+  posts.splice(
+    posts.findIndex((post: PostType) => post.id === currentPostId),
+    1
+  );
   if (posts.length === 0) return null;
   return (
     <div className="RelatedPosts">
       <h3 className="RelatedPosts__title">Articles similaires</h3>
       <div className="divider mb-4"></div>
       <div className="RelatedPosts__grid">
-        {posts?.map((post: PostType, index) => (
+        {posts?.slice(0, 3).map((post: PostType, index) => (
           <div key={index}>
             <PostCard
               title={post?.title}
