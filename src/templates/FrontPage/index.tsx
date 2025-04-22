68b089a7261936932c457b96142d5e27322ca487
Mathilde Arconte <signup@marconte.fr>
2025-04-22 12:24:22 +0200

    feat(Testimonials): ajouter un hook pour récupérer les témoignages et mettre à jour la page d'accueil

--- src/templates/FrontPage/index.tsx
+++ src/templates/FrontPage/index.tsx
@@ -3,13 +3,20 @@ import HeroSection from "../../components/HeroSection";
 import Layout from "../../components/Layout";
 import React from "react";
 import Seo from "../../components/Seo";
+import { useSiteCustomPosts } from "../../hooks/use-custom-post";
 const FrontPage = ({ data }) => {
+  const {
+    allWpTestimonial: { edges: testimonials },
+  } = useSiteCustomPosts();
+
+  console.log(testimonials);
   console.log(data);
+  const { heroSection, pageBuilder } = data;
   return (
     <div>
       <Layout type="frontpage">
         <Seo />
-        <HeroSection data={data.heroSection} />
+        <HeroSection data={heroSection} />
         <AllPosts title="Le blog de l'équipage" isHome />
       </Layout>
     </div>
