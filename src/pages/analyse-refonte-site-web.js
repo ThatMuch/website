685e2f1644a0a86729971c8f577f80fdd45f7be0
Mathilde Arconte <signup@marconte.fr>
2025-06-12 16:16:49 +0200

    Refactor code structure for improved readability and maintainability

--- src/pages/analyse-refonte-site-web.js
+++ src/pages/analyse-refonte-site-web.js
@@ -1,12 +1,13 @@
 import { HeroSection } from "../components/LandingRefonte/HeroSection/HeroSection";
+import LinkSection from "../components/LandingRefonte/LinkSection/LinkSection";
 import NumbersSection from "../components/LandingRefonte/NumbersSection/NumbersSection";
 import React from "react";
 import ScoreSection from "../components/LandingRefonte/ScoreSection/ScoreSection";
 
 const AnalyseRefonteSiteWeb = () => {
-	return (
+  return (
     <div className="bg-landing">
-      <div className="container">
+      <div className="container-fluid">
         <HeroSection
           title="Est-ce le moment de refaire  mon site internet ?"
           desc="Nous avons concocté pour vous, une petite évaluation pour répondre à cette question que vous vous êtes sûrement déjà posé : quand faut-il engager la refonte de son site internet ?"
@@ -15,12 +16,13 @@ const AnalyseRefonteSiteWeb = () => {
         />
         <NumbersSection />
         <ScoreSection />
+        <LinkSection />
       </div>
       <div className="d-flex justify-content-center p-2 bg-dark mt-5">
         <span className="uppercase">thatmuch</span>
       </div>
     </div>
   );
-}
+};
 
 export default AnalyseRefonteSiteWeb;
