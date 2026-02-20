56eb79063deb8a61e59ebb93a863caba5d0a51b8
Mathilde Arconte <signup@marconte.fr>
2026-02-20 17:18:22 +0100

    chore: Remove Netlify-specific Gatsby plugins and adapters.

--- gatsby-config.js
+++ gatsby-config.js
@@ -110,7 +110,6 @@ module.exports = {
     `gatsby-transformer-remark`,
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
-    "gatsby-plugin-netlify",
     {
       resolve: "gatsby-source-filesystem",
       options: {
