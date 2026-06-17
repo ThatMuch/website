b1b8d2b3d4dff28b82e0f7e474a5bd136808c6c7
Mathilde Arconte <signup@marconte.fr>
2026-06-17 18:42:20 +0200

    feat: redesign TestimonialCard component with new styling and update header layout in Testimonials section

--- src/components/TestimonialsSection/TestimonialCard.tsx
+++ src/components/TestimonialsSection/TestimonialCard.tsx
@@ -1,5 +1,6 @@
 import "./TestimonialCard.scss";
 
+import { FaStar } from "react-icons/fa";
 import React from "react";
 import { sanitizeHtml } from "../../utils/sanitize";
 
@@ -21,15 +22,24 @@ export default function TestimonialCard({
   const { nom, role, citation, stars } = testimonialContent;
   return (
     <div className="TestimonialCard">
-      <div>
+      <div className="tmw-tcard__stars">
         {Array.from({ length: stars }, (_, index) => (
-          <span key={index}>⭐</span>
+          <FaStar key={index} color="#FDB900" />
         ))}
       </div>
-      <div dangerouslySetInnerHTML={{ __html: sanitizeHtml(citation) }} />
-      <div className="TestimonialCard__author">
-        <strong className="TestimonialCard__name">{nom}</strong>
-        <p className="TestimonialCard__role">{role}</p>
+      <p
+        className="tmw-tcard__quote"
+        dangerouslySetInnerHTML={{ __html: sanitizeHtml(citation) }}
+      />
+      <div className="tmw-tcard__by">
+        <span className="tmw-tcard__initials">
+          {nom[0]}
+          {nom[1]}
+        </span>
+        <div>
+          <div className="tmw-tcard__name">{nom}</div>
+          <div className="tmw-tcard__role">{role}</div>
+        </div>
       </div>
     </div>
   );
